b69774640dd62f98e913e8f1ec0790d85fd6fa14
Nikita Rubocki <nikita.rubocki@gmail.com>
2019-10-21 12:01:52 -0600

    user input added

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
+	"strings"
 	// "io/ioutil"
 	// "encoding/json"
 )
@@ -9,13 +12,8 @@ import (
 // func makePlanets()
 
 func main() {
+	// variable declarations
 	var name = "Solar System"
-	type Planet []struct{
-		Name, Description string
-	}
-	// planets := [9]Planet{
-	// 	Planet{"Mercury", },
-	// }
 	planets := map[string]string{
 		"Mercury": "A very hot planet, closest to the sun.",
 		"Venus": "It's very cloudy here!",
@@ -27,10 +25,17 @@ func main() {
 		"Neptune": "A very cold planet, furthest from the sun.",
 		"Pluto": "I don't care what they say - it's a planet.",
 	}
-	 
+	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Printf("Welcome to the %v!\n", name)
+	fmt.Printf("There are %v planets to explore.\n", len(planets))
+	fmt.Println("What is your name?")
+	//var userName string
+	input, _ := reader.ReadString('\n')
+	userName := strings.TrimSuffix(input, "\n")
+	fmt.Println("Nice to meet you, "+userName+". My name is Eliza, I'm an old friend of Alexa.")
 	
-	fmt.Println(planets["Earth"])
+	// fmt.Println(planets["Earth"])
 }
 
 // file, _ := ioutil.ReadFile("planetarySystem.json")
@@ -42,3 +47,6 @@ func main() {
 	// }
 	//var Space map[string]Planet
 	// var Space System
+	// type Planet []struct{
+	// 	Name, Description string
+	// }
